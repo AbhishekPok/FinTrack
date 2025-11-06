1ec5b13c7e589f0e0d3cd5ad65e91a716a57e5d7
AbhishekPok <abhishekpokhrel001@gmail.com>
2025-11-06 10:27:39 +0545

    setting up in new environment

--- Backend_Expense/expense/expense/settings.py
+++ Backend_Expense/expense/expense/settings.py
@@ -81,16 +81,16 @@ WSGI_APPLICATION = 'expense.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'expense',
-            'USER': 'admin',
-            'PASSWORD': 'Admin1',
-            'HOST': 'localhost',
-            'PORT': '5432',
-        }
-}
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'expense',
+#             'USER': 'admin',
+#             'PASSWORD': 'Admin1',
+#             'HOST': 'localhost',
+#             'PORT': '5432',
+#         }
+# }
 
 
 
